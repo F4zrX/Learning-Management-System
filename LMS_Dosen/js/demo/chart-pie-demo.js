46f2a054fd698004f04ebc3a41257bc32e34a482
Fazri <FazriSu12@gmail.com>
2024-10-22 01:36:40 +0700

    LMS

---
+++ LMS_Dosen/js/demo/chart-pie-demo.js
@@ -0,0 +1,35 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#295982';
+
+// Pie Chart Example
+var ctx = document.getElementById("myPieChart");
+var myPieChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ["Phyton Program", "Kecerdasan Buatan", "Machine Learning", "Forensik Teknologi"],
+    datasets: [{
+      data: [25, 25, 25, 25],
+      backgroundColor: ['#295982', '#6FABD0', '#92C0D7'],
+      hoverBackgroundColor: ['#295982', '#6FABD0', '#92C0D7'],
+      hoverBorderColor: "rgba(234, 236, 244, 1)",
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#295982",
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      caretPadding: 10,
+    },
+    legend: {
+      display: false
+    },
+    cutoutPercentage: 80,
+  },
+});
